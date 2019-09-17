b6c428c3691a22a2b29997233ac955fc33eed1b5
vbhone <1124920238@qq.com>
2019-09-17 11:43:24 +0800

    2019.9.17

--- src/servlet/FirstServlet.java
+++ src/servlet/FirstServlet.java
@@ -1,21 +1,21 @@
 package servlet;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
 @WebServlet(name = "TestServlet")
-public class FirstServlet extends javax.servlet.http.HttpServlet {
+public class FirstServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String developer = request.getParameter("developer");
         response.setContentType("test/html; charset=UTF-8");
         response.getWriter().println("欢迎开发者" + developer);
     }
-
-
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-//        String developer = request.getParameter("developer");
-//        response.setContentType("test/html; charset=UTF-8");
-//        response.getWriter().println("欢迎开发者" + developer);
-    }
 }
+
+
+
+
